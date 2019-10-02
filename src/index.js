67f1c09dd771e3f0fb2d31e773f2bb32866724cf
Marcel Coelho <flasd2@gmail.com>
2019-10-02 11:54:07 -0300

    style: fix linting problems

--- src/index.js
+++ src/index.js
@@ -3,16 +3,17 @@ import blessed from 'blessed';
 import Viewer from './lib/viewer';
 import Command from './lib/command';
 
-export default function(commandList, screenTitle = "foobar") {
+export default function (commandList, screenTitle = 'foobar') {
   const screen = blessed.screen({ smartCSR: true });
   let coms = [];
 
   screen.title = screenTitle;
 
   // Quit on Escape, q, or Control-C.
-  screen.key(['q', 'C-c'], function (ch, key) {
+  screen.key(['q', 'C-c'], (/* ch, key */) => {
     Promise.all(coms.map((com) => com.shutdown()))
       .catch((err) => {
+        // eslint-disable-next-line no-console
         console.error(err);
         process.exit(1);
       })
@@ -20,23 +21,23 @@ export default function(commandList, screenTitle = "foobar") {
   });
 
   // Focus our element.
-  const width = `${100 / commandList.length | 0}%`;
-  
+  const width = `${100 / commandList.length || 0}%`;
+
   coms = commandList.map((command, i) => {
-    const com =  { command };
-    
+    const com = { command };
+
     const boxOpt = {
       ...(com.boxOpt || {}),
       width,
-      left: i ? `${i * 100 / commandList.length | 0}%` : '0',
+      left: i ? `${(i * 100) / commandList.length || 0}%` : '0',
     };
-    
+
     const c = new Command(com.command, com.opt || {}, boxOpt);
     c.createBox();
     screen.append(c.box);
     return c;
   });
-  
+
   const viewer = new Viewer(coms);
   viewer.createBox();
   screen.append(viewer.box);
@@ -45,9 +46,8 @@ export default function(commandList, screenTitle = "foobar") {
     viewer.refresh();
     screen.render();
   }, 100);
-  
+
   coms.forEach((com) => {
     com.run(delayedRender);
   });
-};
-
+}
